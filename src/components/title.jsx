c252184d949df065e4621fb16dae0f78e8b0b295
Chitty Liu <d2921118@gmail.com>
2018-12-19 19:41:13 +0000

    initial commit with the basic components

---
+++ src/components/title.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Title extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather Finder</h1>
+        <p>A weather forcasting app using Open Weather API</p>
+      </div>
+    );
+  }
+}
+
+export default Title;
