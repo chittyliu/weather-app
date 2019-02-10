a56417f73ed923e923aa54019a5aa99a0528551c
Chitty Liu <d2921118@gmail.com>
2019-02-10 13:10:53 +0800

    initial state done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import Form from "./compoments/Form";
 import Weather from "./compoments/Weather";
 
 const API_KEY = "d0a10211ea3d36b0a6423a104782130e";
-let location = "london";
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    humidity: undefined,
+    city: undefined,
+    country: undefined,
+    description: undefined,
+    error: undefined
+  };
   getWeather = async e => {
     e.preventDefault();
+    const city = e.target.elements.city.value;
     const api_call = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
     );
     const data = await api_call.json();
     console.log(data);
